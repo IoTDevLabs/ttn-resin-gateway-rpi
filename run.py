e5faf85d677716b4ad9a4fce5835dd9e12e712ce
Greg Toth <gtoth@>
2018-10-08 22:01:45 -0400

    shorten loop timeout to 5 sec

--- run.py
+++ run.py
@@ -370,4 +370,5 @@ while True:
 
   # Start forwarder
   subprocess.call(['/opt/ttn-gateway/mp_pkt_fwd', '-c', '/opt/ttn-gateway/', '-s', os.getenv('SPI_SPEED', '8000000')])
-  time.sleep(15)
+  #time.sleep(15)
+  time.sleep(5)
