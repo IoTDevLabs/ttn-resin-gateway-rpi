81648b3c2f703cbd0d67015feff2e907fa0c1d51
Greg Toth <gtoth@>
2018-10-10 17:41:24 -0400

    use forked packet_forwarder

--- dev/build.sh
+++ dev/build.sh
@@ -43,7 +43,8 @@ else
 fi
 
 if [ ! -d packet_forwarder ]; then
-    git clone https://github.com/kersing/packet_forwarder.git  || { echo 'Cloning packet forwarder failed.' ; exit 1; }
+    #git clone https://github.com/kersing/packet_forwarder.git  || { echo 'Cloning packet forwarder failed.' ; exit 1; }
+    git clone https://github.com/IoTDevLabs/packet_forwarder.git  || { echo 'Cloning packet forwarder failed.' ; exit 1; }
 else
     cd packet_forwarder
     git reset --hard
